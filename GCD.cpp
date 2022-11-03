7356e394cdd60516611fc3c21093bac84177e842
Apurv Anand <93825178+apurvanand-2002@users.noreply.github.com>
2022-11-03 13:46:26 +0530

    Add files via upload

---
+++ GCD.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int GCD(int a, int b)
+{
+    if (a % b == 0)
+    {
+        return b;
+    }
+    else
+    {
+        return GCD(b, a % b);
+    }
+}
+int main()
+{
+    cout << GCD(9, 3) << endl;
+}
